c0a5e5e6e2211ddb6b290584b018c8e8393ffd37
nycspies <121522894+nycspies@users.noreply.github.com>
2025-01-14 13:18:53 -0500

    Update __init__.py

--- hypebot/__init__.py
+++ hypebot/__init__.py
@@ -1,4 +1,13 @@
-from .handlers import HypeBot
+from redbot.core import commands
+
+class HypeBot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def hype(self, ctx):
+        await ctx.send("Let's get hyped!")
 
 async def setup(bot):
-  await bot.add_cog(HypeBot(bot))
+    cog = HypeBot(bot)
+    await bot.add_cog(cog)  # Make sure this is awaited!
